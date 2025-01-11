8a1c6182ad8567e43ef3ac624d0b74db42b5501c
Jean Andreotti Borges da Silva <jeandreotti25@gmail.com>
2025-01-11 18:47:19 -0300

    atualizado api

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.querySelector('.busca').addEventListener('submit', async(event)=>{
 
 // um do git - let url =`http://api.openweathermap.org/geo/1.0/direct?q=${encodeURI(input)}&limit=1&appid=58ce985714a31aec823c26d9d7dbdde0`;
 
-let url =`http://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=bf866371d57cbfdba200dde031558ce2&units=metric&lang=pt_br` //professor
+let url =  `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=9cffd38ea43bbf2bc472c8cfcba06120&units=metric&lang=pt_br` //professor
 
 let results = await fetch(url);
 let json = await results.json();
